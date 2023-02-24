0c9f12072ee96e7d9cf3d071551229e2fde68ade
GeorgiaM93 <105521641+GeorgiaM93@users.noreply.github.com>
2023-02-24 23:47:46 +0000

    Commit

--- starter/script.js
+++ starter/script.js
@@ -20,23 +20,56 @@ function getCoordinates(city) {
         .then(function (data) {
             //console.log(data);
             getCuurentWeatherData(data[0].lat, data[0].lon, city);
+            getFiveDaysForecast(data[0].lat, data[0].lon);
         })
 }
 
 function getCuurentWeatherData(lat, lon, city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
             console.log(data);
-            //getCuurentWeatherData(data[0].lat, data[0].lon);
 
             var h1 = $("<h1>");
             h1.text(`${city} (${moment().format('M/D/YYYY')})`);
 
-            var temp = $("<p>"); //need to carry on from here and do same thing with humidity etc but when the tutor will send the one that works
-            temp.text(`Temp: ${data.weather[0].main.temp}`);
-            $("#today").append(h1, temp);
+            var temp = $("<p>");
+            var wind = $("<p>");
+            var humidity = $("<p>");
+            temp.text(`Temp: ${data.main.temp}`);
+            wind.text(`wind: ${data.wind.speed}`)
+            humidity.text(`humidity: ${data.main.humidity}`)
+            $("#today").append(h1, temp, wind, humidity);
+
         })
+}
+function getFiveDaysForecast(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var temp1 = data.list[5].main.temp
+            var temp2 = data.list[13].main.temp
+            var temp3 = data.list[21].main.temp
+            var temp4 = data.list[29].main.temp
+            var temp5 = data.list[37].main.temp
+            var wind1 = data.list[5].wind.speed
+            var wind2 = data.list[13].wind.speed
+            var wind3 = data.list[21].wind.speed
+            var wind4 = data.list[29].wind.speed
+            var wind5 = data.list[37].wind.speed
+            var humidity1 = data.list[5].main.humidity
+            var humidity2 = data.list[13].main.humidity
+            var humidity3 = data.list[21].main.humidity
+            var humidity4 = data.list[29].main.humidity
+            var humidity5 = data.list[37].main.humidity
+            var heading1 = $("<h1>");
+            heading1.text = data.list[5].dt_txt
+            heading1.text = data.list[13].dt_txt
+        })
+
 }
